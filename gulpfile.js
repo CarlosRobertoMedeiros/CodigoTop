9543ea0dd2f2305dc2fe420682488977abb1cf1a
Carlos Roberto Medeiros de Lima <carlosmedeiroslima@gmail.com>
2019-03-26 19:51:29 -0300

    Corrigindo o problema de politica de browser

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+ 
+gulp.task('webserver', function() {
+  gulp.src("/Aula 4")
+    .pipe(webserver({
+      host:"localhost",
+      port:8020,
+      livereload: true,
+      directoryListing: false,
+      open: true,
+      /*fallback:"hello.js"*/
+    }));
+});
